9c5cc97347bacfeb5a2c932d8721fa299f15abc4
Mahesh Natarajan <umamags@gmail.com>
2017-09-04 11:57:30 +0530

    added config

--- importbillsms/scripts/02_create_tables.sql
+++ importbillsms/scripts/02_create_tables.sql
@@ -3,7 +3,7 @@ create table public.invoice
     invoice_id integer NOT NULL,
     invoice_name character varying(50) COLLATE pg_catalog."default" NOT NULL,
     invoice_location character varying(200) COLLATE pg_catalog."default" NOT NULL,
-    invoice_creation_date timestamp COLLATE pg_catalog."default" NOT NULL
+    invoice_creation_date timestamp NOT NULL,
     PRIMARY KEY(invoice_id)
     )
 WITH (
